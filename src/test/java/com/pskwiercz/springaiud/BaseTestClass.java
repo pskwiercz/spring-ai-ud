02d9552195375d36862dbc435aee411643365b8e
Przemek Skwiercz <przemek.skwiercz@gmail.com>
2024-06-19 18:05:20 +0200

    Prompt Engineering 1

---
+++ src/test/java/com/pskwiercz/springaiud/BaseTestClass.java
@@ -0,0 +1,21 @@
+package com.pskwiercz.springaiud;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ChatClientConfiguration.class)
+public class BaseTestClass {
+
+    @Autowired
+    ChatClient openAiChatClient;
+
+    String chat(String prompt) {
+
+        return openAiChatClient
+                .prompt(new PromptTemplate(prompt).create())
+                .call()
+                .content();
+    }
+}
---
+++ src/test/java/com/pskwiercz/springaiud/ChatClientConfiguration.java
@@ -0,0 +1,17 @@
+package com.pskwiercz.springaiud;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class ChatClientConfiguration {
+
+    @Autowired
+    ChatClient.Builder builder;
+    @Bean
+    public ChatClient getChatClient() {
+        return builder.build();
+    }
+}
